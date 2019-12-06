1291accc0fbd21f856d362bf07bd65505f05fcde
fywalter <feiyuwalter@gmail.com>
2019-12-06 16:27:05 +0100

    add faster version

--- task4/prepare_fft_features_1.py
+++ task4/prepare_fft_features_1.py
@@ -3,8 +3,8 @@ import os
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-"""
-def extract_and_save_fft(train_eeg1, train_eeg2, train_emg, mode):
+
+def extract_and_save_fft_old(train_eeg1, train_eeg2, train_emg, mode):
     if not os.path.exists('./data'):
         os.mkdir('./data')
     sample_rate = 128
@@ -15,19 +15,23 @@ def extract_and_save_fft(train_eeg1, train_eeg2, train_emg, mode):
     for name, signals in zip(names, [train_eeg1, train_eeg2, train_emg]):
         print("Preparing {} {} fft features...".format(mode, name))
         fft_features = []
+        if name in ['eeg1', 'eeg2']:
+            cut_len = 48
+        else:
+            cut_len = 60
         for sig in tqdm(signals):
-            sig_fft_feature = np.zeros((96, 16))
-            for slice_idx in range(slice_size):
+            sig_fft_feature = np.zeros((cut_len, slice_size+1))
+            for slice_idx in range(slice_size+1):
                 start_pos = slice_idx*step_size
                 slice = sig[start_pos:start_pos+window_size]
                 slice_fft = np.abs(np.fft.rfft(slice)/window_size)
-                slice_fft = slice_fft[:96]  # 0 - 48 Hz
-                sig_fft_feature[:, slice_idx] = np.reshape(slice_fft, (96, 1))[:, 0]
+                slice_fft = slice_fft[:cut_len]  # 0 - 48 Hz
+                sig_fft_feature[:, slice_idx] = np.reshape(slice_fft, (cut_len, 1))[:, 0]
+            # print(sig_fft_feature.shape)
             fft_features.append(sig_fft_feature)
         fft_features = np.array(fft_features)
         np.save('./data/{}_{}_fft_features.npy'.format(mode, name), fft_features)
     return
-"""
 
 
 def extract_and_save_fft(train_eeg1, train_eeg2, train_emg, mode):
@@ -66,8 +70,8 @@ def main():
     test_eeg2 = np.load('./data/test_eeg2.npy')
     test_emg = np.load('./data/test_emg.npy')
 
-    extract_and_save_fft(train_eeg1, train_eeg2, train_emg, 'train')
-    extract_and_save_fft(test_eeg1, test_eeg2, test_emg, 'test')
+    extract_and_save_fft_old(train_eeg1, train_eeg2, train_emg, 'train')
+    extract_and_save_fft_old(test_eeg1, test_eeg2, test_emg, 'test')
 
     return
 
